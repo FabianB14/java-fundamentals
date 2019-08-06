b11d3c899f6bc9c2b064b54eff28ecc63b0b6b15
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-08-06 13:56:52 -0700

    completed all

--- basics/Main.java
+++ basics/Main.java
@@ -1,3 +1,6 @@
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
   public static void main(String[]args) {
     System.out.println("Hello  Superworld");
@@ -9,6 +12,8 @@ System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".")
 
 int turtleCount = 0;
 System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+    flipNHeads(3);
+    clock();
   }
 
   public static String pluralize(String str, int num){
@@ -18,5 +23,36 @@ System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCoun
     else{
       return (str+"s");
     }
+  }
+
+  public static void flipNHeads(int num){
+    int totalCount = 0;
+    int headsCount = 0;
+
+    while(headsCount <= num){
+      double flipped = Math.random();
+      if(flipped >= .5){
+        System.out.println("Heads");
+        headsCount++;
+      }
+      else{
+        System.out.println("Tails");
+      }
+      totalCount++;
+    }
+    System.out.println("It took " + totalCount + " " + pluralize("flip", totalCount) + " to get " + headsCount + " " + pluralize("head", headsCount) + " " + "in a row.");
+  }
+  public static void clock(){
+    while(0==0){
+      try {
+      TimeUnit.SECONDS.sleep(1);
+      LocalTime localTime = LocalTime.now();
+      System.out.println(localTime.getHour() + ":" + localTime.getMinute()+ ":" + localTime.getSecond());
+      }
+      catch(InterruptedException e){
+        System.out.println("wait");
+      }
+    }
+  
   }
 }
