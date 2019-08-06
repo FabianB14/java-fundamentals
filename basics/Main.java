8312072415e5530899afcd9cb4c833b395684f2e
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-08-06 10:35:50 -0700

    this is the pluralized feature complete

--- basics/Main.java
+++ basics/Main.java
@@ -1,5 +1,22 @@
 public class Main {
   public static void main(String[]args) {
     System.out.println("Hello  Superworld");
+    int dogCount = 1;
+System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+int catCount = 2;
+System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+int turtleCount = 0;
+System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+  }
+
+  public static String pluralize(String str, int num){
+    if(num == 1){
+      return str;
+    }
+    else{
+      return (str+"s");
+    }
   }
 }
