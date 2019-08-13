92c9906edfdefba00ae5955dcd7b940f376c35a9
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-08-13 15:01:02 -0700

    finshed the test

--- inheritance/src/main/java/inheritance/Restaurant.java
+++ inheritance/src/main/java/inheritance/Restaurant.java
@@ -1,12 +1,12 @@
 package inheritance;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Restaurant {
     String name;
     int stars;
     String priceRange;
-    List <Reviews> reviews;
+    ArrayList <Reviews> reviews = new ArrayList<>();
 
     public Restaurant(String name, int stars, String princeRange){
         this.name = name;
@@ -20,6 +20,7 @@ public class Restaurant {
     public void updateStars(Reviews review){
         int sum = 0;
         for(int i = 0; i < this.reviews.size();i++){
+            System.out.println(this.reviews.get(i).stars);
             sum += this.reviews.get(i).stars;
         }
         this.stars = sum/this.reviews.size();
--- inheritance/src/test/java/inheritance/RestaurantTest.java
+++ inheritance/src/test/java/inheritance/RestaurantTest.java
@@ -9,11 +9,16 @@ public class RestaurantTest {
     @Test
     public void restaurantToString() {
         Restaurant McDonalds = new Restaurant("McDonald's",0,"$");
-        assertEquals("this should equal the string of the restaurant", "McDonald's has 0 starts and is priced at $",
+        assertEquals("this should equal the string of the restaurant", "McDonald's has 0 stars and is priced at $",
                 McDonalds.restaurantToString());
     }
 
     @Test
     public void updateStars() {
+        Restaurant McDonalds = new Restaurant("McDonald's",0,"$");
+        Reviews newReview = new Reviews("Fabian",4,"It was super awesome here",McDonalds);
+        Reviews newReview2 = new Reviews("Fabian2",3,"It was super awesome here",McDonalds);
+        newReview.addReview(McDonalds);
+        assertEquals("this is going to be the new stars", "Fabian2 Says: It was super awesome here and also rated McDonald's with 3",newReview2.addReview(McDonalds));
     }
 }
